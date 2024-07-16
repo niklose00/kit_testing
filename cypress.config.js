67d62c16b5a3ada3b38a0fc16defb8d2cd951e5f
Nico <klosenico00@gmail.com>
2024-07-16 10:48:21 +0200

    final zum Testen

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,25 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'vx7m77',
+  projectId: 'vx7m77',  // Ihre Projekt-ID
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    overwrite: false,
+    html: false,
+    json: true
+  },
+  video: true, // Videos von den Tests erstellen
+  screenshotsFolder: 'cypress/screenshots',
+  videosFolder: 'cypress/videos',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+      // Event listeners oder Plugins hier hinzufügen
+      on('after:run', (results) => {
+        if (results) {
+          console.log('Tests completed with status:', results.totalFailed > 0 ? 'Failed' : 'Passed');
+        }
+      });
+    }
+  }
 });
